f19eaae82f522fb7a472a96f0ba31ec920e906cf
Noah Addino <Noah.gr.addino@gmail.com>
2022-12-13 22:13:37 +0100

    up

--- installer.lua
+++ installer.lua
@@ -1,16 +1,19 @@
 local params = {...}
-local item = params[1]
-if item == "menu" then
+if params[1] == "menu" then
 	shell.run("rm menu.lua")
 	shell.run("wget https://github.com/Marcaday/ComputerCraft/raw/main/menu.lua menu.lua")
-elseif item == "remote_control" then
+elseif params[1] == "remote_control" then
 	shell.run("rm menu.lua")
 	shell.run("wget https://github.com/Marcaday/ComputerCraft/raw/main/remote_control.lua remote_control.lua")
-elseif	item == "remote_control_receiver" then
+elseif	params[1] == "remote_control_receiver" then
 	shell.run("rm menu.lua")
 	shell.run("wget https://github.com/Marcaday/ComputerCraft/raw/main/remote_control_receiver.lua remote_control_receiver.lua")
-else if item == "installer" then
+else if params[1] == "installer" then
 	shell.run("rm installer.lua")
 	shell.run("wget https://github.com/Marcaday/ComputerCraft/raw/main/installer.lua installer.lua")
+else if params[1] == "plugin" and params[2] == "main_redstone_control" then
+	shell.run("rm mControlPlugins/main_redstone_control.lua")
+	shell.run("wget https://github.com/Marcaday/ComputerCraft/raw/main/plugins_main_redstone_control.lua mControlPlugins/main_redstone_control.lua")
+
 end
 end
