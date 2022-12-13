a273c7d40e0f40e7bbfeded7691a4f8552ec4c56
Noah Addino <Noah.gr.addino@gmail.com>
2022-12-13 22:18:29 +0100

    up

--- installer.lua
+++ installer.lua
@@ -3,10 +3,10 @@ if params[1] == "menu" then
 	shell.run("rm menu.lua")
 	shell.run("wget https://github.com/Marcaday/ComputerCraft/raw/main/menu.lua menu.lua")
 elseif params[1] == "remote_control" then
-	shell.run("rm menu.lua")
+	shell.run("rm remote_control.lua")
 	shell.run("wget https://github.com/Marcaday/ComputerCraft/raw/main/remote_control.lua remote_control.lua")
 elseif	params[1] == "remote_control_receiver" then
-	shell.run("rm menu.lua")
+	shell.run("rm remote_control_receiver.lua")
 	shell.run("wget https://github.com/Marcaday/ComputerCraft/raw/main/remote_control_receiver.lua remote_control_receiver.lua")
 else if params[1] == "installer" then
 	shell.run("rm installer.lua")
