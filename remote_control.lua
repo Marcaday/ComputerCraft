c147c424be564b002e8b51ec806b6c36122ae15f
Noah Addino <Noah.gr.addino@gmail.com>
2022-12-17 07:26:42 +0100

    up

--- remote_control.lua
+++ remote_control.lua
@@ -2,18 +2,40 @@ local lId = os.getComputerID()
 rednet.open("back")
 local lProtocol = "CCMP"
 local lLogP = "MLP"
+lConnectId = -1
 
+local function split(inputstr, sep)
+	if sep == nil then
+		sep = "%s"
+	end
+	local t={}
+	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+		table.insert(t, str)
+	end
+	return (t)
+end
 
 while true do
 	local connected_ids = {rednet.lookup(lProtocol)}
-
-	for i=1, #connected_ids do
-		print(connected_ids[i])
-		rednet.send(connected_ids[i], "identity", lProtocol)
-		rednet.broadcast(lId.."- Asking identity to "..connected_ids[1], lLogP)
+	input = read()
+	tInput = split(input, " ")
+	if input == "ls" and lConnectId == -1 then
+		for i=1, #connected_ids do
+			rednet.send(connected_ids[i], "identity", lProtocol)
+			rednet.broadcast(lId.."- Asking identity to "..connected_ids[1], lLogP)
+			senderID, message = rednet.receive(lProtocol)
+			term.write(senderID.."-"message)
+		end
+	elseif tInput[1] == "connect"  then
+		lConnectId = tonumber(tInput[2])
+		term.write("connect to "..tostring(lConnectId))
+	elseif  input == "exit" and lConnectId ~= -1 then
+		lConnectId = -1
+		term.write("Exiting "..tostring(lConnectId))
+	else	
+		rednet.send(lConnectId, input, lProtocol)
+		rednet.broadcast(lId.."-"..input.." to "..tostring(lConnectId))
 	end
-	senderID, message = rednet.receive(lProtocol)
-	print(message)
-	senderID, message = rednet.receive(lLogP)
-	print(message)
+	end
+	
 end
