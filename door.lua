347d5fe51818c99e3ae6311bdfa458f3036ef7e4
Noah Addino <Noah.gr.addino@gmail.com>
2022-12-17 08:55:51 +0100

    up

---
+++ door.lua
@@ -0,0 +1,28 @@
+local args = {...}
+
+local arg = args[1]
+local senderID = args[2]
+
+
+
+
+function door(arg, senderID)
+	local lId = os.getComputerID()
+	local lErrorP = "MerrorP"
+	local lLogP = "MLP"
+	local lFarmControlSide = "up"
+	if arg == "open" or arg == "Open" then
+		redstone.setOutput(lFarmControlSide, false)
+		rednet.broadcast(lId.."- Maintenance doors are now "..arg, lLogP)
+	elseif arg == "close" or arg == "Close" then
+		redstone.setOutput(lFarmControlSide, true)
+		rednet.broadcast(lId.."- Maintenance doors are now "..arg, lLogP)
+	else
+		rednet.send(senderID, lId.."- Error: Unknown command for Maintenance doors", lErrorP)
+		rednet.broadcast(lId.."- Error: Unknown command for Maintenance doors", lLogP)
+		print("Error: Unknown command")
+	end
+
+end
+
+door(arg, senderID)
