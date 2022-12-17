d25eeb66f650d8a91b407fb75961d433844b089e
Noah Addino <Noah.gr.addino@gmail.com>
2022-12-17 06:39:56 +0100

    up

--- log_tracker.lua
+++ log_tracker.lua
@@ -1,15 +1,25 @@
 local lId = os.getComputerID()
+x,y = 1, 1
 native = term.current()
 monitor = peripheral.find("monitor")
 if monitor then
-	term.redirect(monitor)
+ monitor.setTextScale(0.5)
+    term.redirect(monitor)
+term.clear()
+ w, h = term.getSize()
+ native.write("w"..w.."h"..h)
 end
-rednet.open("bottom")
+rednet.open("top")
 local identity = "RemoteController"
 local lLogP = "MLP"
 rednet.host(lLogP, identity)
 while true do 
-	senderID, message = rednet.receive(lLogP)
-	term.write(senderID.."-"..message)
-	native.write(senderID.."-"..message)
+    senderID, message = rednet.receive(lLogP)
+    term.write(message)
+term.setCursorPos(x, y+2)
+if y == (h) then
+term.clear()
+y=1
+end
+y = y+2
 end
