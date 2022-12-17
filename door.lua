649824e515c6b0197a483cea5b96013cd595effd
Noah Addino <Noah.gr.addino@gmail.com>
2022-12-17 14:18:05 +0100

    up

--- door.lua
+++ door.lua
@@ -13,14 +13,14 @@ function door(arg, senderID)
 	local lFarmControlSide = "up"
 	if arg == "open" or arg == "Open" then
 		redstone.setOutput(lFarmControlSide, false)
-		rednet.broadcast(lId.."- Maintenance doors are now "..arg, lLogP)
+		rednet.broadcast(lId.." - Maintenance doors are now "..arg, lLogP)
 	elseif arg == "close" or arg == "Close" then
 		redstone.setOutput(lFarmControlSide, true)
-		rednet.broadcast(lId.."- Maintenance doors are now "..arg, lLogP)
+		rednet.broadcast(lId.." - Maintenance doors are now "..arg, lLogP)
 	else
 		rednet.send(senderID, lId.."- Error: Unknown command for Maintenance doors", lErrorP)
-		rednet.broadcast(lId.."- Error: Unknown command for Maintenance doors", lLogP)
-		rednet.broadcast(lId.."- Error: Unknown command for Maintenance doors", lErrorP)
+		rednet.broadcast(lId.." - Error: Unknown command for Maintenance doors", lLogP)
+		rednet.broadcast(lId.." - Error: Unknown command for Maintenance doors", lErrorP)
 	end
 
 end
--- kelp_farm.lua
+++ kelp_farm.lua
@@ -13,14 +13,13 @@ function kelp_farm(arg, senderID)
 	local lFarmControlSide = "right"
 	if arg == "on" or arg == "On" then
 		redstone.setOutput(lFarmControlSide, false)
-		rednet.broadcast(lId.."- Kelp farm is i now "..arg, lLogP)
+		rednet.broadcast(lId.." - Kelp farm is i now "..arg, lLogP)
 	elseif arg == "off" or arg == "Off" then
 		redstone.setOutput(lFarmControlSide, true)
-		rednet.broadcast(lId.."- Kelp farm is i now "..arg, lLogP)
+		rednet.broadcast(lId.." - Kelp farm is i now "..arg, lLogP)
 	else
-		rednet.send(senderID, lId.."- Error: Unknown command for kelp_farm", lErrorP)
-		rednet.broadcast(lId.."- Error: Unknown command for kelp_farm", lLogP)
-		print("Error: Unknown command")
+		rednet.send(senderID, lId.." - Error: Unknown command for kelp_farm", lErrorP)
+		rednet.broadcast(lId.." - Error: Unknown command for kelp_farm", lLogP)
 	end
 
 end
