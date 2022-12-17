797f1eeb0393dcfe6c9f5296af0f7bb433483c4f
Noah Addino <Noah.gr.addino@gmail.com>
2022-12-17 08:24:53 +0100

    up

--- remote_control.lua
+++ remote_control.lua
@@ -3,40 +3,42 @@ rednet.open("back")
 local lProtocol = "CCMP"
 local lLogP = "MLP"
 lConnectId = -1
-
+x,y = 1,1
+term.setCursorPos(x,y)
+ 
 local function split(inputstr, sep)
-	if sep == nil then
-		sep = "%s"
-	end
-	local t={}
-	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-		table.insert(t, str)
-	end
-	return (t)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        table.insert(t, str)
+    end
+    return (t)
 end
-
+ 
 while true do
-	lConnectId == -1
-	input = read()
-	tInput = split(input, " ")
-	if input == "ls" and lConnectId == -1 then
-		for i=1, #connected_ids do
-			local connected_ids = {rednet.lookup(lProtocol)}
-			rednet.send(connected_ids[i], "identity", lProtocol)
-			rednet.broadcast(lId.."- Asking identity to "..connected_ids[1], lLogP)
-			senderID, message = rednet.receive(lProtocol)
-			term.write(senderID.."-"message)
-		end
-	elseif tInput[1] == "connect"  then
-		lConnectId = tonumber(tInput[2])
-		term.write("connect to "..tostring(lConnectId))
-	elseif  input == "exit" and lConnectId ~= -1 then
-		lConnectId = -1
-		term.write("Exiting "..tostring(lConnectId))
-	else	
-		rednet.send(lConnectId, input, lProtocol)
-		rednet.broadcast(lId.."-"..input.." to "..tostring(lConnectId))
-	end
-	end
-	
-end
+    input = read()
+    tInput = split(input, " ")
+    if input == "ls" and lConnectId == -1 then
+    local connected_ids = {rednet.lookup(lProtocol)}
+    for i=1, #connected_ids do
+                rednet.send(connected_ids[i], "identity", lProtocol)
+            rednet.broadcast(lId.."- Asking identity to "..connected_ids[1], lLogP)
+            senderID, message = rednet.receive(lProtocol)
+            term.write(senderID.."-"..message)
+			term.setCursorPos(x,y+2)
+        end
+    elseif tInput[1] == "connect"  then
+        lConnectId = tonumber(tInput[2])
+        term.write("connect to "..tostring(lConnectId))
+		term.setCursorPos(x,y+2)
+    elseif  input == "exit" and lConnectId ~= -1 then
+        lConnectId = -1
+        term.write("Exiting "..tostring(lConnectId))
+		term.setCursorPos(x,y+2)
+    else    
+        rednet.send(lConnectId, input, lProtocol)
+        rednet.broadcast(lId.."-"..input.." to "..tostring(lConnectId), lLogP)
+    end
+    end
--- remote_control_receiver.lua
+++ remote_control_receiver.lua
@@ -1,51 +1,51 @@
-rednet.open("top")
+rednet.open("bottom")
 identity = "Default Identity"
-
-
+ 
+ 
 local function split(inputstr, sep)
-	if sep == nil then
-		sep = "%s"
-	end
-	local t={}
-	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-		table.insert(t, str)
-	end
-	return (t)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        table.insert(t, str)
+    end
+    return (t)
 end
-
+ 
 function RemoteReceiver(identity)
-	local lId = os.getComputerID()
-	local lProtocol = "CCMP"
-	local lErrorP = "MerrorP"
-	local lLogP = "MLP"
-	local lSupportedMessages = {"identity", "kelp_farm", "door"}
-	local lSenders_control = {"5", "id"}
-	rednet.host(lProtocol, identity)
-	rednet.host(lErrorP, identity)
-	rednet.host(lLogP, identity)
-	while true do
-		senderID, message = rednet.receive(lProtocol)
-		lmessage =  split(message, " ")
-		command = lmessage[1]
-		args = table.remove(lmessage, 1)
-		for i=1, #lSenders_control do
-			if tostring(senderID) == lSenders_control[i] then
-				for j=1, #lSupportedMessages do
-					if command == lSupportedMessages[j] then 
-						if command == "identity" then
-							rednet.send(senderID, identity, lProtocol)
-							rednet.broadcast(lId.."-Sending Identity: "..identity, lLogP)	
-						else
-							multishell.launch({}, command, args[1], senderID)
-							rednet.broadcast(senderID.."-Asking to run :"..message.." on -"..lId, lLogP)
-						end
-					else
-						rednet.send(senderID, lId.." - Error: Unknown message", lErrorP)
-					end
-				end
-			end
-		end
+    local lId = os.getComputerID()
+    local lProtocol = "CCMP"
+    local lErrorP = "MerrorP"
+    local lLogP = "MLP"
+    local lSupportedMessages = {"identity", "kelp_farm", "door"}
+    local lSenders_control = {"5", "id"}
+    rednet.host(lProtocol, identity)
+    rednet.host(lErrorP, identity)
+    rednet.host(lLogP, identity)
+    while true do
+        senderID, message = rednet.receive(lProtocol)
+        lmessage =  split(message, " ")
+        command = lmessage[1]
+        args = lmessage[2]
+        for i=1, #lSenders_control do
+            if tostring(senderID) == lSenders_control[i] then
+                for j=1, #lSupportedMessages do
+                    if command == lSupportedMessages[j] then
+                        if command == "identity" then
+                            rednet.send(senderID, identity, lProtocol)
+                            rednet.broadcast(lId.."-Sending Identity: "..identity, lLogP)   
+                        else
+                            multishell.launch({}, command..".lua", args, senderID)
+                            rednet.broadcast(senderID.."-Asking to run :"..message.." on -"..lId, lLogP)
+                        end
+                    else
+                        rednet.send(senderID, lId.." - Error: Unknown message", lErrorP)
+                    end
+                end
+            end
+        end
 end
 end
-
+ 
 RemoteReceiver(identity)
