28f612b7b8c6ff3ea2cb9fb7dd10af2542955aaa
Noah Addino <Noah.gr.addino@gmail.com>
2022-12-17 08:58:11 +0100

    up

--- installer.lua
+++ installer.lua
@@ -14,9 +14,9 @@ else if params[1] == "installer" then
 else if params[1] == "kelp_farm" then
 	shell.run("rm kelp_farm.lua")
 	shell.run("wget https://github.com/Marcaday/ComputerCraft/raw/main/kelp_farm.lua kelp_farm.lua")
-else if params[1] == "kelp_farm" then
-	shell.run("rm kelp_farm.lua")
-	shell.run("wget https://github.com/Marcaday/ComputerCraft/raw/main/kelp_farm.lua door_controllua")
+else if params[1] == "door" then
+	shell.run("rm door.lua")
+	shell.run("wget https://github.com/Marcaday/ComputerCraft/raw/main/door.lua door.lua")
 else if params[1] == "log_tracker" then
 	shell.run("rm log_tracker.lua")
 	shell.run("wget https://github.com/Marcaday/ComputerCraft/raw/main/log_tracker.lua log_tracker.lua")
