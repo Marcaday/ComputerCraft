3f3e38bc1bfcb5fabc57844b1dda175ecbc5386e
Noah Addino <Noah.gr.addino@gmail.com>
2022-12-19 04:52:17 +0100

    up

--- reactor_screen.lua
+++ reactor_screen.lua
@@ -9,8 +9,8 @@ term.clear()
 
 local rStatus
 local rTemperature 
-local cButtonUpleftCornerx, cButtonUpleftCornery = 1, 6
-local cButtonDownrightCornerx, cButtonDownrightCornery = 4, 9
+local cButtonStartx, cButtonStarty = 1, 6
+local cButtonSizex, cButtonSizey = 4, 9
 function drawStatusWindow(wStatusWindow, rStatus, rTemperature)
 	x, y= 1, 1
 	wStatusWindow.setCursorPos(x,y)
@@ -25,18 +25,22 @@ function drawButtonWindow(wButtonWindow, rStatus)
 	if rStatus then
 		wButtonWindow.setBackgroundColor(colors.lime)
 		wButtonWindow.clear()
-		wButtonWindow.setCursorPos(cButtonUpleftCornerx, cButtonUpleftCornery)
+		wButtonWindow.setCursorPos(cButtonStartx, cButtonStarty)
 		wButtonWindow.write("ON")
 	else
 		wButtonWindow.setBackgroundColor(colors.red)
 		wButtonWindow.clear()
-		wButtonWindow.setCursorPos(cButtonUpleftCornerx, cButtonUpleftCornery)
+		wButtonWindow.setCursorPos(cButtonStartx, cButtonStarty)
 		wButtonWindow.write("OFF")
 	end
 end
 
 function check_button(x,y)
-	if y >= cButtonUpleftCornery and y <= cButtonDownrightCornery and x<= cButtonDownrightCornerx and x>= cButtonUpleftCornerx then
+	if y >= cButtonStarty 
+	and y <= cButtonStarty + cButtonSizey
+	and x >=  cButtonStartx
+	and x >= cButtonStartx + cButtonSizex 
+	then
 		if rStatus then
 			reactor.scram()
 		else
@@ -46,7 +50,7 @@ function check_button(x,y)
 
 end
 wStatusWindow = window.create(term.current(), 1, 1,  mWidth, 6)
-wButtonWindow = window.create(term.current(), cButtonUpleftCornerx, cButtonUpleftCornery,  cButtonDownrightCornerx, cButtonDownrightCornery)
+wButtonWindow = window.create(term.current(), cButtonStartx, cButtonStarty,  cButtonSizex, cButtonSizey)
 while true do
 	rStatus = reactor.getStatus()
 	rTemperature = reactor.getTemperature()-273.15
@@ -56,6 +60,6 @@ while true do
 	local event, button, cx, cy = os.pullEvent()
 	rStatus = reactor.getStatus()
 	if event == "monitor_touch" then 
-		check_button(cx, cy, mWidth, mHeight, rStatus)
+		check_button(cx, cy)
 	end
 end
